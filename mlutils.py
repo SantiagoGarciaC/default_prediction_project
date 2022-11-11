f9413a1e0106ab766a59ff45d3ce9f2dc668873e
SantiagoGarciaC <santiago.garcia13@udea.edu.co>
2022-11-11 02:52:05 -0500

    plotting poo fc

---
+++ mlutils.py
@@ -0,0 +1,40 @@
+class plot_learning_curve():
+  import matplotlib.pyplot as plt
+  import numpy as np
+  from sklearn.model_selection import learning_curve
+  def __init__(self,estimator,X,y,cv):
+    self.estimator = estimator
+    self.X = X
+    self.y = y
+    self.cv = cv
+
+
+  def plot(self): 
+    train_sizes, train_scores, test_scores = learning_curve(self.estimator, self.X, self.y, cv=self.cv, shuffle=True)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    # Plot learning curve
+    plt.grid()
+    plt.fill_between(
+            train_sizes,
+            train_scores_mean - train_scores_std,
+            train_scores_mean + train_scores_std,
+            alpha=0.1,
+            color="r",
+        )
+    plt.fill_between(
+            train_sizes,
+            test_scores_mean - test_scores_std,
+            test_scores_mean + test_scores_std,
+            alpha=0.1,
+            color="g",
+        )
+    plt.plot(train_sizes, train_scores_mean, "o-", color="r", label="train score")
+    plt.plot(train_sizes, test_scores_mean, "o-", color="g", label="test score")
+    plt.legend(loc="best")
+    plt.ylabel("Score")
+    plt.xlabel("Training examples")
+    plt.title(self.estimator.__class__.__name__)
+    return plt.show()
